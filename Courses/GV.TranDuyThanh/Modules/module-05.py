ec74c71d789b59c3c52ca452398dfb419f4f54ba
lekhoide <65319249+lekhoide@users.noreply.github.com>
2021-05-20 08:19:31 +0700

    Learn on 20/05/2021

--- Courses/GV.TranDuyThanh/Modules/module-05.py
+++ Courses/GV.TranDuyThanh/Modules/module-05.py
@@ -97,71 +97,115 @@ def CheckPrime(x):
     trình bày một số hàm quan trọng xử lý chuỗi của Python
 """
 
-name_cau4 = " Lê Khôi Đệ"
+# name_cau4 = " Lê Khôi Đệ"
 # upper đưa chuỗi về in HOA
 # lower đưa chuỗi về in thường
-print(name_cau4.upper())
-print(name_cau4.lower())
+# print(name_cau4.upper())
+# print(name_cau4.lower())
 
 # rjust căn lề phải
 # ljust căn lề trái
 # center căn giữa
-word_4 = " ABCD"
-print(word_4.rjust(10,"*"))
+# word_4 = " ABCD"
+# print(word_4.rjust(10,"*"))
 # Output: ABCD nếu số ký tự chèn nhỏ hơn chuỗi gốc thì không có gì thay đổi
-print(word_4.rjust(3,"*"))
+# print(word_4.rjust(3,"*"))
 # Output: ABCD nếu số ký tự muốn chèn nhỏ hơn Chuỗi gốc thì không có gì thay đổi
-print(word_4.ljust(1))
-print(word_4.ljust(10,'*'))
+# print(word_4.ljust(1))
+# print(word_4.ljust(10,'*'))
 # Output: ABCD số lượng căn giữa mà nhỏ hơn số ký tự gốc thì không có gì thay đổi
-print(word_4.center(10,'*'))
+# print(word_4.center(10,'*'))
 
 # strip: hàm xóa khoảng trắng dư thừa
-print(word_4.__len__())
-word_4 = word_4.strip()
-print(word_4.__len__())
+# print(word_4.__len__())
+# word_4 = word_4.strip()
+# print(word_4.__len__())
 
 # startsWith: kiểm tra chuỗi có bắt đầu bằng một chuỗi con nào đó hay không
 # endsWith: kiểm tra chuỗi có kết thúc bằng một chuỗi con nào đó hay không
-print(word_4.startswith("A"))
-print(word_4.endswith("D"))
+# print(word_4.startswith("A"))
+# print(word_4.endswith("D"))
 
 # find: trả về vị trí đầu tiên tìm thấy
 # rfind: trả về vị trí cuối cùng tìm thấy. Nếu không thấy sẽ trả về -1
-print(word_4.find('C'))
-print(word_4.rfind(''))
+# print(word_4.find('C'))
+# print(word_4.rfind(''))
 
 # Hàm count trả về số lần xuât hiện của Chuỗi con trong Chuỗi gốc, không tồn tại trả về 0
-print(word_4.count("A"))
+# print(word_4.count("A"))
 
 # formant sử dụng {} để dành chỗ xuất dữ liệu
-a_4 = 5
-b_4 = 9
-c_4 = a_4/b_4
-word_5 = "{0}/{1}={2}".format(a_4,b_4,c_4)
-print(word_5)
+# a_4 = 5
+# b_4 = 9
+# c_4 = a_4/b_4
+# word_5 = "{0}/{1}={2}".format(a_4,b_4,c_4)
+# print(word_5)
 
 # substring
-word_6 = "Hello World!"
-print(word_6[2:])
-print(word_6[:2])
-print(word_6[:-2])
-print(word_6[-2:])
-print(word_6[2:-2])
-print(word_6[6:11])
+# word_6 = "Hello World!"
+# print(word_6[2:])
+# print(word_6[:2])
+# print(word_6[:-2])
+# print(word_6[-2:])
+# print(word_6[2:-2])
+# print(word_6[6:11])
 
 # split: dùng để tách chuỗi thành mảng các chuỗi con
-word_7 = "sv007;Nguyễn Thị Tẹt;1/1/1999"
-arr_4 = word_7.split(';')
-for x in arr_4:
-    print(x)
+# word_7 = "sv007;Nguyễn Thị Tẹt;1/1/1999"
+# arr_4 = word_7.split(';')
+# for x in arr_4:
+#     print(x)
 
 # join: dùng để nối chuỗi
-word_8 = ','
-word_8 = word_8.join(arr_4)
-print(word_8)
+# word_8 = ','
+# word_8 = word_8.join(arr_4)
+# print(word_8)
 
+"""
+    Câu 5: Xử lý chuỗi với hàm cơ bản
+    Viết chương trình nhập vào một chuối
+    - Bao nhiêu chữ in HOA
+    - Bao nhiêu chữ in thường
+    - Bao nhiêu chữ là chữ số
+    - Bao nhiêu chữ là ký tự đặc biệt
+    - Bao nhiêu chữ là khoảng trắng
+    - Bao nhiêu chữ là nguyên âm
+    - Bao nhiêu chữ là phụ âm
+"""
 
+def checkChuoi(str):
+    inHoa = 0
+    inThuong = 0
+    chuSo = 0
+    khoangTrang = 0
+    kyTuDB = 0
+    nguyenAm = 0
+    phuAm = 0
+    for i in str:
+        if i.isupper():
+            inHoa += 1
+        if i.islower():
+            inThuong += 1
+        if i.isdigit():
+            chuSo += 1
+        if not i.isalnum():
+            kyTuDB += 1
+        if i.isspace():
+            khoangTrang += 1
+        if i in ['a', 'ă', 'â', 'e', 'ê', 'i', 'o', 'ô', 'ơ', 'u', 'ư', 'y']:
+            nguyenAm += 1
+        else:
+            phuAm += 1
+    print("Có",inHoa,"chữ in hoa")
+    print("Có",inThuong,"chữ in thường")
+    print("Có",chuSo,"chữ số")
+    print("Có",kyTuDB,"ký tự đặc biệt")
+    print("Có",khoangTrang,"chữ khoảng trắng")
+    print("Có",nguyenAm,"nguyên âm")
+    print("Có",phuAm,"phụ âm")   
+
+str_5 = "Lê Khôi Đệ 05 &"
+checkChuoi(str_5)
 
 
 
