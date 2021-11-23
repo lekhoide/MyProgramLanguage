3ed58945cb95d6c43eabbdc48a3b960871af2962
lekhoide <lekhoide@gmail.com>
2021-11-23 07:05:38 +0700

    learn-stack

---
+++ Courses/Hutech/B3-stack-and-queue/queue.js
@@ -0,0 +1,26 @@
+/**
+ * Ví dụ: xếp hàng mua vé
+ * Gồm nhiều phần tử có thứ tự
+ * Hoạt động theo cơ chế "Vào trước, ra trước" (FIFO - First In First Out)
+ * Phần tử được đặt vào hàng đợi sớm nhất sẽ được lấy ra đầu tiên
+ * 
+ * Có 2 cách để xây dựng hàng đợi:
+ * - Mảng 1 chiều
+ * - Danh sách liên kết đơn
+ * Các thao tác cơ bản
+ */
+
+class Queue {
+    constructor() {
+
+    }
+    // Kiểm tra hàng đợi rỗng
+
+    // Kiểm tra hàng đợi đầy
+
+    // Thêm một phần tử vào cuối hàng đợi
+
+    // Lấy ra một phần tử từ đầu Queue
+
+    // Kiểm tra phần tử đầu và phần tử cuối
+}
