9a34710fcd7ffb88a5db2e6b34c276b07827b483
lekhoide <lekhoide@gmail.com>
2022-02-23 10:45:32 +0700

    learn python basic - P6

---
+++ Courses/Basic/TDT/Source Code/6_KhaiNiemVeHam/LyThuyet.py
@@ -0,0 +1,25 @@
+"""
+    1. Khái niệm về hàm(function)
+
+    2. Cấu trúc tổng quát của hàm: def name (paramenter list)
+                                        block
+    3. Cách gọi hàm
+
+    4. Nguyên tắc hoạt động của hàm: LIFO
+
+    5. Viết tài liệu cho hàm
+
+    6. Global Variable: global
+
+    7. Parameter mặc định
+
+    8. Lambda Expression: lambda parameterlist : expression
+    - lambda: là từ khóa
+    - paramaterlist:
+    - expression: biểu thức đơn trong Python(ko nhập complex)
+
+    9. Giới thiệu hàm về Đệ qui
+    - Hàm gọi là chính nó
+    - Điểm dừng của bài toán + Biết được quy luật của bài toán
+
+"""
